af39e0a084ed4dcbb6b1a6abb52fcdc46995aea7
yuanye <yyzhou@91jkys.com>
2017-05-24 10:46:08 +0800

    1.拆分出分类，优化逻辑。

--- ZBToastHUD/ZBToastHUD/ZBToastHUD/ZBToastHUD.m
+++ ZBToastHUD/ZBToastHUD/ZBToastHUD/ZBToastHUD.m
@@ -8,6 +8,110 @@
 
 #import "ZBToastHUD.h"
 
+#pragma mark - UIColor+ZBToastHUD
+
+@interface UIColor (ZBToastHUD)
+
++ (UIColor *)zb_colorWithValue:(NSUInteger)rgbValue;
+
++ (UIColor *)zb_colorWithValue:(NSUInteger)rgbValue alpha:(CGFloat)alpha;
+
+@end
+
+@implementation UIColor (ZBToastHUD)
+
++ (UIColor *)zb_colorWithValue:(NSUInteger)rgbValue
+{
+    return [UIColor zb_colorWithValue:rgbValue alpha:1.0];
+}
+
++ (UIColor *)zb_colorWithValue:(NSUInteger)rgbValue alpha:(CGFloat)alpha
+{
+    return [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0
+                           green:((float)((rgbValue & 0xFF00) >> 8))/255.0
+                            blue:((float)(rgbValue & 0xFF))/255.0
+                           alpha:alpha];
+}
+
+@end
+
+#pragma mark - NSMutableAttributedString+ZBToastHUD
+
+@interface NSMutableAttributedString (ZBToastHUD)
+
++ (NSMutableAttributedString *)zb_attributedStringWithText:(NSString *)text font:(UIFont *)font lineSpacing:(CGFloat)lineSpacing;
+
++ (CGFloat)zb_attributedStringHeightWithText:(NSString *)text font:(UIFont *)font lineSpacing:(CGFloat)lineSpacing width:(CGFloat)width;
+
++ (CGFloat)zb_attributedStringWidthWithText:(NSString *)text font:(UIFont *)font lineSpacing:(CGFloat)lineSpacing height:(CGFloat)height;
+
+@end
+
+@implementation NSMutableAttributedString (ZBToastHUD)
+
++ (NSMutableAttributedString *)zb_attributedStringWithText:(NSString *)text font:(UIFont *)font lineSpacing:(CGFloat)lineSpacing
+{
+    if (!text) text = @"";
+    if (!font) font = [UIFont systemFontOfSize:17.0];
+    if (lineSpacing < 0) lineSpacing = 0;
+    
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.lineSpacing = lineSpacing;
+    NSDictionary *attributes = @{
+                                 NSFontAttributeName : font,
+                                 NSParagraphStyleAttributeName : paragraphStyle
+                                 };
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:text attributes:attributes];
+    
+    return attributedString;
+}
+
++ (CGFloat)zb_attributedStringHeightWithText:(NSString *)text font:(UIFont *)font lineSpacing:(CGFloat)lineSpacing width:(CGFloat)width
+{
+    if (!text) text = @"";
+    if (!font) font = [UIFont systemFontOfSize:17.0];
+    if (lineSpacing < 0) lineSpacing = 0;
+    if (width < 0) width = 0;
+    
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.lineSpacing = lineSpacing;
+    NSDictionary *attributes = @{
+                                 NSFontAttributeName : font,
+                                 NSParagraphStyleAttributeName : paragraphStyle
+                                 };
+    CGFloat height = [text boundingRectWithSize:CGSizeMake(width, MAXFLOAT)
+                                        options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
+                                     attributes:attributes
+                                        context:nil].size.height;
+    
+    return height;
+}
+
++ (CGFloat)zb_attributedStringWidthWithText:(NSString *)text font:(UIFont *)font lineSpacing:(CGFloat)lineSpacing height:(CGFloat)height
+{
+    if (!text) text = @"";
+    if (!font) font = [UIFont systemFontOfSize:17.0];
+    if (lineSpacing < 0) lineSpacing = 0;
+    if (height < 0) height = 0;
+    
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.lineSpacing = lineSpacing;
+    NSDictionary *attributes = @{
+                                 NSFontAttributeName : font,
+                                 NSParagraphStyleAttributeName : paragraphStyle
+                                 };
+    CGFloat width = [text boundingRectWithSize:CGSizeMake(MAXFLOAT, height)
+                                       options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
+                                    attributes:attributes
+                                       context:nil].size.width;
+    
+    return width;
+}
+
+@end
+
+#pragma mark - ZBToastHUD
+
 static const CGFloat ZBToastHUDToastDismissDuration = 1.5;
 static NSString *const ZBToastHUDLoadingAnimationKey = @"rotationAnimation";
 
@@ -24,7 +128,6 @@ static NSString *const ZBToastHUDLoadingAnimationKey = @"rotationAnimation";
 @property (nonatomic, strong) UIImageView *toastImageView;
 @property (nonatomic, strong) UILabel *toastMessageLabel;
 
-//
 @property (nonatomic, assign) BOOL isLoadingHUD;
 
 @end
@@ -75,12 +178,12 @@ static NSString *const ZBToastHUDLoadingAnimationKey = @"rotationAnimation";
         {
             self.loadingImageView.image = [UIImage imageNamed:@"ZBToastHUD.bundle/ZBToastHUD_LoadingStyleLight"];
             self.loadingHUDView.backgroundColor = [UIColor clearColor];
-            self.loadingMessageLabel.textColor = [self colorWithValue:0x999999];
+            self.loadingMessageLabel.textColor = [UIColor zb_colorWithValue:0x999999];
         }
         else
         {
             self.loadingImageView.image = [UIImage imageNamed:@"ZBToastHUD.bundle/ZBToastHUD_LoadingStyleDark"];
-            self.loadingHUDView.backgroundColor = [self colorWithValue:0x333333 alpha:0.8];
+            self.loadingHUDView.backgroundColor = [UIColor zb_colorWithValue:0x333333 alpha:0.8];
             self.loadingHUDView.layer.cornerRadius = 4.0;
             self.loadingHUDView.layer.masksToBounds = YES;
             self.loadingMessageLabel.textColor = [UIColor whiteColor];
@@ -118,7 +221,7 @@ static NSString *const ZBToastHUDLoadingAnimationKey = @"rotationAnimation";
         
         if (self.toastImageView.image)
         {
-            CGFloat toastMessageLabelWidth = [self attributedStringWidthWithLabelHeight:13 text:self.toastMessageLabel.text font:self.toastMessageLabel.font lineSpacing:0.0];
+            CGFloat toastMessageLabelWidth = [NSMutableAttributedString zb_attributedStringWidthWithText:self.toastMessageLabel.text font:self.toastMessageLabel.font lineSpacing:0.0 height:13];
             if (toastMessageLabelWidth <= 96-24) {
                 toastMessageLabelWidth = 96-24;
             }
@@ -133,7 +236,7 @@ static NSString *const ZBToastHUDLoadingAnimationKey = @"rotationAnimation";
         }
         else
         {
-            CGFloat toastMessageLabelWidth = [self attributedStringWidthWithLabelHeight:15 text:self.toastMessageLabel.text font:self.toastMessageLabel.font lineSpacing:0.0];
+            CGFloat toastMessageLabelWidth = [NSMutableAttributedString zb_attributedStringWidthWithText:self.toastMessageLabel.text font:self.toastMessageLabel.font lineSpacing:0.0 height:15];
             // single line
             if (toastMessageLabelWidth <= screenWidth*0.75-24)
             {
@@ -146,9 +249,9 @@ static NSString *const ZBToastHUDLoadingAnimationKey = @"rotationAnimation";
             else
             {
                 toastMessageLabelWidth = screenWidth*0.75-24;
-                CGFloat toastMessageLabelHeight = [self attributedStringHeightWithLabelWidth:toastMessageLabelWidth text:self.toastMessageLabel.text font:self.toastMessageLabel.font lineSpacing:3.0];
+                CGFloat toastMessageLabelHeight = [NSMutableAttributedString zb_attributedStringHeightWithText:self.toastMessageLabel.text font:self.toastMessageLabel.font lineSpacing:3.0 width:toastMessageLabelWidth];
                 
-                NSMutableAttributedString *messageAttributedText = [self attributedStringWithText:self.toastMessageLabel.text font:self.toastMessageLabel.font lineSpacing:3.0];
+                NSMutableAttributedString *messageAttributedText = [NSMutableAttributedString zb_attributedStringWithText:self.toastMessageLabel.text font:self.toastMessageLabel.font lineSpacing:3.0];
                 self.toastMessageLabel.attributedText = messageAttributedText;
                 self.toastMessageLabel.textAlignment = NSTextAlignmentCenter;
                 
@@ -160,91 +263,6 @@ static NSString *const ZBToastHUDLoadingAnimationKey = @"rotationAnimation";
         }
     }
     
-}
-
-#pragma mark - private method
-
-- (void)startRotationAnimation
-{
-    CABasicAnimation *rotationAnimation;
-    rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
-    rotationAnimation.fromValue = @(0);
-    rotationAnimation.toValue = @(M_PI * 2.0);
-    rotationAnimation.duration = 1.0;
-    rotationAnimation.cumulative = YES;
-    rotationAnimation.repeatCount = HUGE_VAL;
-    rotationAnimation.delegate = self;
-    rotationAnimation.removedOnCompletion = NO;
-    rotationAnimation.fillMode = kCAFillModeForwards;
-    [self.loadingImageView.layer addAnimation:rotationAnimation forKey:ZBToastHUDLoadingAnimationKey];
-}
-
-- (void)stopRotationAnimation
-{
-    [self.loadingImageView.layer removeAnimationForKey:ZBToastHUDLoadingAnimationKey];
-}
-
-- (UIColor *)colorWithValue:(NSUInteger)rgbValue
-{
-    return [self colorWithValue:rgbValue alpha:1.0];
-}
-
-- (UIColor *)colorWithValue:(NSUInteger)rgbValue alpha:(CGFloat)alpha
-{
-    return [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0
-                           green:((float)((rgbValue & 0xFF00) >> 8))/255.0
-                            blue:((float)(rgbValue & 0xFF))/255.0
-                           alpha:alpha];
-}
-
-- (NSMutableAttributedString *)attributedStringWithText:(NSString *)text font:(UIFont *)font lineSpacing:(CGFloat)lineSpacing
-{
-    if (!text) text = @"";
-    if (!font) font = [UIFont systemFontOfSize:17.0];
-    if (lineSpacing < 0) lineSpacing = 0;
-    
-    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-    paragraphStyle.lineSpacing = lineSpacing;
-    NSDictionary *attributes = @{NSFontAttributeName:font, NSParagraphStyleAttributeName:paragraphStyle};
-    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:text attributes:attributes];
-    
-    return attributedString;
-}
-
-- (CGFloat)attributedStringHeightWithLabelWidth:(CGFloat)width text:(NSString *)text font:(UIFont *)font lineSpacing:(CGFloat)lineSpacing
-{
-    if (width < 0) width = 0;
-    if (!text) text = @"";
-    if (!font) font = [UIFont systemFontOfSize:17.0];
-    if (lineSpacing < 0) lineSpacing = 0;
-    
-    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-    paragraphStyle.lineSpacing = lineSpacing;
-    NSDictionary *attributes = @{NSFontAttributeName:font, NSParagraphStyleAttributeName:paragraphStyle};
-    CGFloat height = [text boundingRectWithSize:CGSizeMake(width, MAXFLOAT)
-                                        options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
-                                     attributes:attributes
-                                        context:nil].size.height;
-    
-    return height;
-}
-
-- (CGFloat)attributedStringWidthWithLabelHeight:(CGFloat)height text:(NSString *)text font:(UIFont *)font lineSpacing:(CGFloat)lineSpacing
-{
-    if (height < 0) height = 0;
-    if (!text) text = @"";
-    if (!font) font = [UIFont systemFontOfSize:17.0];
-    if (lineSpacing < 0) lineSpacing = 0;
-    
-    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-    paragraphStyle.lineSpacing = lineSpacing;
-    NSDictionary *attributes = @{NSFontAttributeName:font, NSParagraphStyleAttributeName:paragraphStyle};
-    CGFloat width = [text boundingRectWithSize:CGSizeMake(MAXFLOAT, height)
-                                       options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
-                                    attributes:attributes
-                                       context:nil].size.width;
-    
-    return width;
 }
 
 #pragma mark - public method
@@ -394,6 +412,28 @@ static NSString *const ZBToastHUDLoadingAnimationKey = @"rotationAnimation";
     }
 }
 
+#pragma mark - private method
+
+- (void)startRotationAnimation
+{
+    CABasicAnimation *rotationAnimation;
+    rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    rotationAnimation.fromValue = @(0);
+    rotationAnimation.toValue = @(M_PI * 2.0);
+    rotationAnimation.duration = 1.0;
+    rotationAnimation.cumulative = YES;
+    rotationAnimation.repeatCount = HUGE_VAL;
+    rotationAnimation.delegate = self;
+    rotationAnimation.removedOnCompletion = NO;
+    rotationAnimation.fillMode = kCAFillModeForwards;
+    [self.loadingImageView.layer addAnimation:rotationAnimation forKey:ZBToastHUDLoadingAnimationKey];
+}
+
+- (void)stopRotationAnimation
+{
+    [self.loadingImageView.layer removeAnimationForKey:ZBToastHUDLoadingAnimationKey];
+}
+
 #pragma mark - getter
 
 - (UIView *)loadingHUDView
@@ -427,7 +467,7 @@ static NSString *const ZBToastHUDLoadingAnimationKey = @"rotationAnimation";
 {
     if (!_toastHUDView) {
         _toastHUDView = [[UIView alloc] init];
-        _toastHUDView.backgroundColor = [self colorWithValue:0x333333 alpha:0.8];
+        _toastHUDView.backgroundColor = [UIColor zb_colorWithValue:0x333333 alpha:0.8];
         _toastHUDView.layer.cornerRadius = 4.0;
         _toastHUDView.layer.masksToBounds = YES;
     }
